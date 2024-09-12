ae6830457e0f91216a266c511fc9fa9d16db6742
Umadevijunju <149047990+Umadevijunju@users.noreply.github.com>
2024-09-12 11:41:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+let inputBox = document.getElementById('inputBox')
+let buttons = document.querySelectorAll('button')
+
+let string =''
+
+
+buttons .forEach(element=>{
+    element.addEventListener('click',(b)=>{
+        if(b.target.innerText == '='){
+            string = (eval(string))
+            inputBox.value = string;
+             
+        }
+        else if(b.target.innerText =='Ac'){
+            string = ''
+            inputBox.value = string; 
+        }
+        else if(b.target.innerText=='DEL'){
+            string = string .substring(0,string.length-1)
+            inputBox.value = string;
+        }
+          else if(b.target.id == 'plusMinus') {
+             string = string (-eval(string))
+             inputBox.value = string;
+        }
+
+        else{
+            string +=b.target.innerText
+            inputBox.value = string
+        }
+    })
+})
